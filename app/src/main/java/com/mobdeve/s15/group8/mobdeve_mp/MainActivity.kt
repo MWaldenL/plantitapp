051678bf25051d4a685d13e6b2deef8c144d6f17
Matthew Lua <luamatthew@gmail.com>
2021-08-11 14:06:53 +0800

    Implement Google signin

--- app/src/main/java/com/mobdeve/s15/group8/mobdeve_mp/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s15/group8/mobdeve_mp/MainActivity.kt
@@ -1,11 +1,81 @@
 package com.mobdeve.s15.group8.mobdeve_mp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts.StartActivityForResult
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var textView: TextView
+    private lateinit var btnLogin: SignInButton
+    private lateinit var btnSignOut: Button
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+    private val launcher = registerForActivityResult(StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+            try {
+                val account: GoogleSignInAccount? = task.getResult(ApiException::class.java)
+                Log.d("HATDOG", "firebaseAuthWithGoogle:" + account?.id)
+                firebaseAuthWithGoogle(account?.idToken!!)
+                textView.text = account.displayName
+            } catch (e: ApiException) {
+                Log.e("TAG","signInResult:failed code=" + e.statusCode)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    Log.d("HATDOG", user.toString())
+                } else {
+                    Log.w("HATDOG", "signInWithCredential: failure", task.exception)
+                }
+            }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        auth = Firebase.auth
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        btnLogin = findViewById(R.id.btn_login)
+        btnSignOut = findViewById(R.id.button_signout)
+        textView = findViewById(R.id.textView)
+        btnLogin.setOnClickListener {
+            launcher.launch(mGoogleSignInClient.signInIntent)
+        }
+        btnSignOut.setOnClickListener {
+            Log.d("HATDOG", "signed out")
+            textView.text = "no user"
+            Firebase.auth.signOut()
+            mGoogleSignInClient.signOut()
+        }
     }
 }
