f0aa858c54ca884433796e053b3400d6ff089b5a
Byron <byron.nill10@gmail.com>
2021-08-20 16:00:36 +0800

    manual trigger feedback done

--- app/src/main/java/com/mobdeve/s15/group8/mobdeve_mp/controller/activities/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s15/group8/mobdeve_mp/controller/activities/MainActivity.kt
@@ -4,18 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.DialogFragment
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.firestore.FieldValue
 import com.mobdeve.s15.group8.mobdeve_mp.singletons.F
 import com.mobdeve.s15.group8.mobdeve_mp.singletons.GoogleSingleton
 import com.mobdeve.s15.group8.mobdeve_mp.R
+import com.mobdeve.s15.group8.mobdeve_mp.controller.activities.fragments.dialogs.AppFeedbackDialogFragment
+import com.mobdeve.s15.group8.mobdeve_mp.model.services.DBService
+import java.util.*
 
-class MainActivity: AppCompatActivity() {
+class MainActivity:
+    AppCompatActivity(),
+    AppFeedbackDialogFragment.AppFeedbackDialogListener
+{
     private lateinit var bottomNav: BottomNavigationView
     private lateinit var fabAddPlant: FloatingActionButton
+    private lateinit var btnTrigger: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,5 +40,59 @@ class MainActivity: AppCompatActivity() {
             val addPlantIntent = Intent(this@MainActivity, AddPlantActivity::class.java)
             startActivity(addPlantIntent)
         }
+
+        btnTrigger = findViewById(R.id.btn_trigger)
+        btnTrigger.setOnClickListener {
+            val fragment = AppFeedbackDialogFragment()
+            fragment.show(supportFragmentManager, "feedback")
+        }
+    }
+
+    override fun onFeedbackContinue(
+        dialog: DialogFragment,
+        feedbackRating: Float,
+        feedbackComment: String
+    ) {
+        val id = F.auth.currentUser?.uid
+        val toAdd = hashMapOf(
+            "rating" to feedbackRating,
+            "comment" to feedbackComment
+        )
+
+        DBService.updateDocument(
+            F.usersCollection,
+            id,
+            "feedback",
+            FieldValue.arrayUnion(toAdd)
+        )
+    }
+
+    override fun onFeedbackStop(dialog: DialogFragment) {
+        val id = F.auth.currentUser?.uid
+
+        DBService.updateDocument(
+            F.usersCollection,
+            id,
+            "feedback_stop",
+            true
+        )
+    }
+
+    override fun onFeedbackCancel(dialog: DialogFragment) {
+        val id = F.auth.currentUser?.uid
+
+        DBService.updateDocument(
+            F.usersCollection,
+            id,
+            "feedback_stop",
+            false
+        )
+
+        DBService.updateDocument(
+            F.usersCollection,
+            id,
+            "feedback_last_sent",
+            Date()
+        )
     }
 }
