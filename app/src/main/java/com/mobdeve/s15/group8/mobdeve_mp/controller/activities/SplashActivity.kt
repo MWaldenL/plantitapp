994ab68ca54470be50953de437542adb76b8c11d
Keith Gabriel Badulis <keith_gabriel_badulis@dlsu.edu.ph>
2021-08-24 17:02:25 +0800

    revamped grid view

--- app/src/main/java/com/mobdeve/s15/group8/mobdeve_mp/controller/activities/SplashActivity.kt
+++ app/src/main/java/com/mobdeve/s15/group8/mobdeve_mp/controller/activities/SplashActivity.kt
@@ -12,7 +12,10 @@ import com.mobdeve.s15.group8.mobdeve_mp.model.repositories.PlantRepository
 class SplashActivity : AppCompatActivity(), DBCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // TODO: move to application level to avoid MediaManager is already initialized error
         MediaManager.init(this) // initializer for Cloudinary's image upload
+
         if (F.auth.currentUser == null) { // no user - go to login
             startActivity(Intent(this@SplashActivity, LoginActivity::class.java))
             finish()
