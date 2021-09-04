6ccd2f8c18737534a1a90e5b71037039cff98f72
Byron <byron.nill10@gmail.com>
2021-09-05 01:33:22 +0800

    delete journal dialog

--- app/src/main/java/com/mobdeve/s15/group8/mobdeve_mp/controller/activities/fragments/dialogs/DeleteJournalDialogFragment.kt
+++ app/src/main/java/com/mobdeve/s15/group8/mobdeve_mp/controller/activities/fragments/dialogs/DeleteJournalDialogFragment.kt
@@ -4,12 +4,17 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.widget.Button
 import androidx.fragment.app.DialogFragment
+import com.mobdeve.s15.group8.mobdeve_mp.R
 import java.lang.ClassCastException
 
 class DeleteJournalDialogFragment:
     DialogFragment()
 {
+    private lateinit var btnDeleteJournal: Button
+    private lateinit var btnDeleteJournalCancel: Button
+
     internal lateinit var listener: DeleteJournalDialogListener
 
     interface DeleteJournalDialogListener {
@@ -29,16 +34,23 @@ class DeleteJournalDialogFragment:
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
+            val inflater = requireActivity().layoutInflater
+            val view = inflater.inflate(R.layout.dialog_delete_journal, null)
 
-            builder
-                .setMessage("You are about to delete this journal entry. This action cannot be undone.")
-                .setPositiveButton("Delete") { dialog, id ->
-                    listener.onJournalDelete(this)
-                }
-                .setNegativeButton("Cancel") { dialog, id ->
-                    listener.onJournalDeleteCancel(this)
-                }
-                .create()
+            btnDeleteJournal = view.findViewById(R.id.btn_delete_journal)
+            btnDeleteJournalCancel = view.findViewById(R.id.btn_delete_journal_cancel)
+
+            btnDeleteJournal.setOnClickListener {
+                listener.onJournalDelete(this)
+                this.dismiss()
+            }
+
+            btnDeleteJournalCancel.setOnClickListener {
+                listener.onJournalDeleteCancel(this)
+                this.dismiss()
+            }
+
+            builder.setView(view).create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
 }
