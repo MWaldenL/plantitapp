45a7d2de67471b56fb1f24e398d7c86de0714345
Byron <byron.nill10@gmail.com>
2021-09-04 23:17:40 +0800

    removed disable notif notif

--- app/src/main/java/com/mobdeve/s15/group8/mobdeve_mp/controller/services/NotificationService.kt
+++ app/src/main/java/com/mobdeve/s15/group8/mobdeve_mp/controller/services/NotificationService.kt
@@ -2,10 +2,7 @@ package com.mobdeve.s15.group8.mobdeve_mp.controller.services
 
 import android.content.Context
 import androidx.work.*
-import com.mobdeve.s15.group8.mobdeve_mp.model.workers.DisablingNotificationWorker
 import com.mobdeve.s15.group8.mobdeve_mp.model.workers.FirstNotificationWorker
-import com.mobdeve.s15.group8.mobdeve_mp.model.workers.NotificationWorker
-import java.util.*
 import java.util.concurrent.TimeUnit
 
 object NotificationService {
@@ -13,16 +10,6 @@ object NotificationService {
         val workManager = WorkManager.getInstance(context)
 
         val dailyWorkRequest = OneTimeWorkRequestBuilder<FirstNotificationWorker>()
-            .setInitialDelay(3000, TimeUnit.MILLISECONDS)
-            .build()
-
-        workManager.enqueue(dailyWorkRequest)
-    }
-
-    fun sendDisablingNotification(context: Context) {
-        val workManager = WorkManager.getInstance(context)
-
-        val dailyWorkRequest = OneTimeWorkRequestBuilder<DisablingNotificationWorker>()
             .setInitialDelay(3000, TimeUnit.MILLISECONDS)
             .build()
 
