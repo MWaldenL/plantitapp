61fabd6d0795f510812db19ebe10891a45ccda3c
Matthew Lua <matthew_walden_lua@dlsu.edu.ph>
2021-09-06 01:22:14 +0800

    Add drawable-v21 assets

--- app/src/main/java/com/mobdeve/s15/group8/mobdeve_mp/controller/activities/fragments/DashboardFragment.kt
+++ app/src/main/java/com/mobdeve/s15/group8/mobdeve_mp/controller/activities/fragments/DashboardFragment.kt
@@ -58,6 +58,7 @@ class DashboardFragment : Fragment(), DBCallback {
         taskGroupAdapter.updateData(mTasks)
         mExpandIncompleteGroups()
         btnProfile.setOnClickListener {
+            Log.d("MPDashboardFragment", "btnProfile clicked")
             startActivity(Intent(this@DashboardFragment.activity, ProfileActivity::class.java))
         }
     }
